8075cf47538a594a9cef830e85e3dda2295055bb
rasunaaa <2311104043@ittelkom-pwt.ac.id>
2025-03-04 01:11:17 +0700

    Ignore .vs folder

---
+++ OneDrive/Dokumen/tugas smt 4/KPL/shafiq/shafiq/DoonMachine.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shafiq
+{
+
+    class DoorMachine
+    {
+        private bool isLocked = false;
+        private bool isOpen = false;
+
+        public void CurrentState()
+        {
+            Console.WriteLine($"Pintu {(isLocked ? "terkunci" : "tidak terkunci")} dan {(isOpen ? "terbuka" : "tertutup")}");
+        }
+
+        public void ToggleLock()
+        {
+            isLocked = !isLocked;
+            Console.WriteLine($"Pintu sekarang {(isLocked ? "terkunci" : "tidak terkunci")}");
+        }
+
+        public void OpenDoor()
+        {
+            if (isLocked)
+            {
+                Console.WriteLine("Tidak bisa membuka pintu, karena terkunci!");
+            }
+            else
+            {
+                isOpen = true;
+                Console.WriteLine("Pintu berhasil dibuka.");
+            }
+        }
+
+        public void CloseDoor()
+        {
+            isOpen = false;
+            Console.WriteLine("Pintu telah ditutup.");
+        }
+    }
+}
